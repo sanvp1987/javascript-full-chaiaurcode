a9a2f609c3c3a6e3ccbc6198e6b61767aa108ddb
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-10 05:03:03 +0000

    Operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -17,7 +17,36 @@ let score = "33abc" // NaN - Not a Number
 // console.log(typeof booleanIsLoggedIn)
 // console.log(booleanIsLoggedIn)
 
-let someBoolean = "true"
-let booleanIsString = Boolean(someBoolean)
-console.log(booleanIsString)
-console.log(typeof booleanIsString)
+// let someBoolean = "true"
+// let booleanIsString = Boolean(someBoolean)
+// console.log(booleanIsString)
+// console.log(typeof booleanIsString)
+
+// ***************************** Operations ************************************
+
+// let value = 3
+// let negative = -value
+// console.log(negative)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log((1 + 2) * (5 % 2))
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
