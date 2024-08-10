eecaa0d5fbd03efa4d9e312db5e798044da938ff
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-10 06:06:45 +0000

    comparisons

---
+++ 04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 != 1)
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0) // this is equlity check
+// console.log(null >= 0) // this is comparison check here null value is getting converted as 0. hence true
+// console.log(null)
+
+// console.log(undefined == 0)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0 ) // all 3 always return false
+
+// console.log("2" ===2)
+// console.log("2" == 2)
