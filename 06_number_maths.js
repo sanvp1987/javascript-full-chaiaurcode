b6e42e3df44be9b8c5ec1a7a0c0a6ca8b8cafeb7
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-10 08:55:43 +0000

    number and math

---
+++ 06_number_maths.js
@@ -0,0 +1,31 @@
+ const score = 400
+// console.log(score)
+ const balance =new Number(score)
+
+// console.log(balance)
+// console.log(balance.toFixed(2))
+// console.log(balance.toPrecision())
+// console.log(balance.toString().length)
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-in'))
+
+// console.log(Number.MAX_VALUE)
+
+//+++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-6))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(45))
+// console.log(Math.min(4,76,9,2,6))
+
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max- min + 1)) + min)
