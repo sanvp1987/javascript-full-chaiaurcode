405bb2259964de10f7613b0441a187a28a7e37ac
sanvp1987 <68700853+sanvp1987@users.noreply.github.com>
2024-08-10 04:47:20 +0000

    datatypes & conversionOperation

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,3 +1,6 @@
 let name = "Sandeep"
 let age = 18
 let isLoggedIn = false
+
+console.log(age);
+console.log(isLoggedIn)
---
+++ 03_conversionOperation.js
@@ -0,0 +1,23 @@
+//let score = "33"
+//let score = null // null conversion becomes 0
+let score = "33abc" // NaN - Not a Number
+//let score = undefined // null conversion becomes 0
+
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumner = Number(score)
+
+// console.log(typeof valueInNumner)
+// console.log(valueInNumner)
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someBoolean = "true"
+let booleanIsString = Boolean(someBoolean)
+console.log(booleanIsString)
+console.log(typeof booleanIsString)
